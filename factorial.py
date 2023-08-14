91c13d385bed4b2848d3adf7ae6b9593de30e150
Keerthanaakula2 <kits2001@gmail.com>
2023-08-14 10:47:59 -0400

    added factorial.py file

--- factorial.py
+++ factorial.py
@@ -4,13 +4,14 @@ Factorial Module
 This module provides a function to calculate the factorial of a given non-negative integer.
 """
 
+
 def factorial(num):
     """
     Calculate the factorial of a non-negative integer.
-    
+
     Args:
         num (int): The non-negative integer for which to calculate the factorial.
-    
+
     Returns:
         int: The factorial of the input integer.
     """
